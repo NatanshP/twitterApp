fea09d4b84c73853a130bf177059275b47877d4d
Natansh <natansh.prasad@housing.com>
2020-10-11 20:59:25 +0530

    small fix

--- src/components/tweet/index.js
+++ src/components/tweet/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import moment from 'moment'
 import abbreviateNumber from '../../helpers/convertNumber'
 import processMsg from '../../helpers/processMsg'
@@ -9,7 +9,8 @@ import './style.scss'
 
 const insertProps = (to) => {
   return {
-    to: `/search?q=${encodeURIComponent(to)}`
+    to: `/search?q=${encodeURIComponent(to)}`,
+    onClick: (e) => { e.stopPropagation() }
   }
 }
 
@@ -30,6 +31,7 @@ export default function Tweet (props) {
   } = props
   const history = useHistory()
   const [store] = useStore()
+  const stopPropagation = useCallback((e) => e.stopPropagation(), [])
   const {
     params: {
       id: routeId
@@ -64,9 +66,9 @@ export default function Tweet (props) {
           </div>)}
         <div className='msg-cont'>{processMsg(message, Link, insertProps)}</div>
         <div className='action-buttons'>
-          <div className='comment btn'><i className='icon icon-chat' />{!!comments.length && abbreviateNumber(comments.length)}</div>
-          <div className='retweet btn'><i className='icon icon-retweet-1' />{!!retweets && abbreviateNumber(retweets)} </div>
-          <div className='like btn'><i className='icon icon-heart' /> {!!likes && abbreviateNumber(likes)} </div>
+          <div className='comment btn' onClick={stopPropagation}><i className='icon icon-chat' />{!!comments.length && abbreviateNumber(comments.length)}</div>
+          <div className='retweet btn' onClick={stopPropagation}><i className='icon icon-retweet-1' />{!!retweets && abbreviateNumber(retweets)} </div>
+          <div className='like btn' onClick={stopPropagation}><i className='icon icon-heart' /> {!!likes && abbreviateNumber(likes)} </div>
           {!inFocus && <div />}
         </div>
       </div>
